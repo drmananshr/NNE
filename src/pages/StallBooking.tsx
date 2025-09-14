ad4b668bdcae4ad3f096cb0e616f1e0c4df3fc15
Manan Sharma <merevikdoom@gmail.com>
2025-09-14 16:28:52 +0530

    Fix AnimatePresence import error

--- src/pages/StallBooking.tsx
+++ src/pages/StallBooking.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { motion } from 'framer-motion';
+import { motion, AnimatePresence } from 'framer-motion';
 import { useStalls } from '../hooks/useStalls';
 import { 
   Calendar, 
